49d8c4d73c267a6ae518907818683069ed1b2d9d
hzrncik <hzrncik@redhat.com>
2021-07-09 15:09:43 +0200

    peklo

--- test/base_test.go
+++ test/base_test.go
@@ -1,10 +1,9 @@
 //+build e2e
 
-package base
+package test
 
 import (
 	"github.com/Shopify/sarama"
-	"github.com/strimzi/strimzi-canary/test"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -59,7 +58,7 @@ func TestCanaryTopicLiveliness(t *testing.T) {
 		}
 
 		log.Printf("2c ")
-		if !test.IsTopicPresent(canaryTopicName, topics) {
+		if !IsTopicPresent(canaryTopicName, topics) {
 			t.Errorf("%s is not present", canaryTopicName)
 		}
 		log.Printf("2d ")
@@ -114,7 +113,7 @@ func TestMetricServerContentUpdating(t *testing.T) {
 
 	resp, _ := http.Get(httpUrlPrefix + metricsEndpoint)
 	body, _ := ioutil.ReadAll(resp.Body)
-	totalRequestCountT1 := test.ParseCountFromMetrics(string(body))
+	totalRequestCountT1 := ParseCountFromMetrics(string(body))
 	if len(totalRequestCountT1) < 1 {
 		t.Errorf("Content of metric server is not updated as expected")
 	}
@@ -127,7 +126,7 @@ func TestMetricServerContentUpdating(t *testing.T) {
 
 
 	// totalRequestCountT2 stores value produced after defined number of seconds from obtaining totalRequestCountT1
-	totalRequestCountT2 := test.ParseCountFromMetrics(string(body2))
+	totalRequestCountT2 := ParseCountFromMetrics(string(body2))
 	if totalRequestCountT2 <= totalRequestCountT1{
 		t.Errorf("Data are not updated within requested time period %d on endpoint %s", metricServerUpdateTimeInSeconds, metricsEndpoint)
 	}
